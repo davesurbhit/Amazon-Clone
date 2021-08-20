25a387e712548fc56f41421df7a0699d4cc1cc2b
davesurbhit <davesurbhit2001@gmail.com>
2021-08-20 11:23:01 +0530

    User authentication using firebase

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
 import Header from './Header.js';
 import Home from './Home.js';
 import Checkout from './Checkout.js';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Login from './Login';
+import { auth } from './firebase';
+import { useStateValue } from './StateProvider';
 
 
 
 function App() {
+  const [{}, dispatch] = useStateValue();
+
+  useEffect(() => {
+      //Will only run once when the app component loads...
+    auth.onAuthStateChanged(authUser => {
+      console.log('The User is >>> ', authUser);
+
+      if (authUser) {
+        //user has logged in / the user was logged in
+        dispatch({
+          type: 'SET_USER',
+          user: authUser
+        })
+      } else {
+        //the user is logged out
+        dispatch({
+          type: 'SET_USER',
+          user: null
+        })
+      }
+    }) 
+  }, [])
+
   return (
     // BEM
     <Router>
